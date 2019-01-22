e820d3c0283a1de8386e1321490a9d98b5c7d8d6
Lithelli <46689025+Lithelli@users.noreply.github.com>
2019-01-22 09:48:47 -0600

    First commit

---
+++ toDoList/app.js
@@ -0,0 +1,10 @@
+let input = document.getElementsByTagName("input");
+let button = document.getElementsByTagName("button");
+
+
+let list = [", "]
+list
+function addTo(input){
+    list.push(input)
+}
+console.log(addTo())
